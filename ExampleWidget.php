6c573975563bce7ab233c4b910acee522cf491ea
dewaintech <dewa.intech@gmail.com>
2015-06-10 15:36:28 +0700

    Testing

---
+++ ExampleWidget.php
@@ -0,0 +1,12 @@
+<?php
+namespace finasmart\startup;
+/**
+ * This is just an example.
+ */
+class ExampleWidget extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello World!";
+    }
+}
