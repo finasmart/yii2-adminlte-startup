19edbe5f37720ae44e6c360df9040e2fe3556a1e
dewaintech <dewa.intech@gmail.com>
2015-06-11 00:43:12 +0700

    add AssetBundle

---
+++ web/AdminLteAsset.php
@@ -0,0 +1,22 @@
+<?php
+namespace finasmart\web;
+
+use yii\base\Exception;
+use yii\web\AssetBundle;
+use dmstr\web;
+/**
+ * AdminLte AssetBundle
+ * @since 0.1
+ */
+class AdminLtePluginAsset extends AdminLteAsset
+{
+    public $sourcePath = '@vendor/almasaeed2010/adminlte/plugins';
+    public $css = [
+       'iCheck/all.css',
+    ]; 
+    public $js = [
+		'slimScroll/jquery.slimscroll.min.js',
+		'fastclick/fastclick.min.js',
+		'iCheck/icheck.min.js',
+    ];
+}
